72f31cc17e7bc8e9caf8ac659d66f651ecececa5
Anatoly Plotnikov <aplotnikov@broniboy.ru>
2023-05-04 07:38:50 +0300

    cancel multiple tasks with admin actions

--- backend/task_manager/admin.py
+++ backend/task_manager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django_celery_results.admin import TaskResultAdmin
+from django_celery_results.models import TaskResult
 
-# Register your models here.
+from backend.celery import app
+
+
+@admin.action(description='Cancel selected tasks')
+def cancel_tasks(modeladmin, request, queryset):
+    tasks_id_to_cancel = list(queryset.values_list("task_id", flat=True))
+    app.control.revoke(tasks_id_to_cancel, terminate=True)
+
+
+class ArticleAdmin(TaskResultAdmin):
+    actions = [cancel_tasks]
+
+
+admin.site.unregister(TaskResult)
+admin.site.register(TaskResult, ArticleAdmin)

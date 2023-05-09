79b71b4d51916b8ccc0b0ef204e873efb3a891f4
Anatoly Plotnikov <aplotnikov@broniboy.ru>
2023-05-09 09:29:50 +0300

    add fake task

--- backend/task_manager/tasks.py
+++ backend/task_manager/tasks.py
@@ -1,9 +1,16 @@
+import time
+
 from backend.celery import app
 
 
 @app.task(bind=True, autoretry_for=(Exception,))
-def fake_task(self, param1=None, param2=None, retry=30, delay=10):
+def task_one(self, param1=None, param2=None, retry=5, delay=30):
     try:
         1 / 0
     except Exception as exc:
         raise self.retry(exc=exc, countdown=delay, max_retries=retry)
+
+
+@app.task
+def task_two(*args, **kwargs):
+    time.sleep(100)
